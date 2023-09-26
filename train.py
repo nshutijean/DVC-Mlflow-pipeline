77e4d48e15594b9360c3926991f0aa2ea38d5aa8
nshutijean <j.nshuti73@gmail.com>
2023-09-26 12:53:45 +0200

    data url setting & metrics eval

--- train.py
+++ train.py
@@ -15,6 +15,18 @@ import mlflow
 import mlflow.sklearn
 import dvc.api
 
-# logging warning messages
+# log warning messages
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
+
+# get data url from DVC
+data_url = dvc.api.get_url(
+    path='data/car_evaluation_processed.csv',
+    repo= '.',
+    version = 'v1'
+)
+
+# define metrics for model perfromance evaluatio
+def model_eval(actual, pred):
+    accuracy = accuracy_score(actual, pred)
+    return accuracy
